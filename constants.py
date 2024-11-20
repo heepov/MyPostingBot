fee9386948f766d343a2c1ff5fbf82df2d6e8850
Nikita Arkhipov <heeepov@gmail.com>
2024-11-20 23:30:23 +0300

    pull

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Токен бота
+BOT_TOKEN = "7537652378:AAGi4hDZrGQwbChmfnV3js6jcO5DQNm4Ufg"
+
+ADMIN_ID = 6449563889
+
+# Путь к файлу для хранения постов
+FILE_PATH_USER_DATA = "user_data.json"
+FILE_PATH_POSTS = "posts.json"
+
+
+MAX_MEDIA_IN_GROUP = 10
+DATE_TIME_FORMAT = "%Y-%m-%d %H:%M"
+DATE_TIME_FORMAT_PRINT = "YYYY-MM-DD HH:MM"
